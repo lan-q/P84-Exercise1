a650675dbf43592678a8b35bff836f750c8e362b
lvshijie88 <43091261+lvshijie88@users.noreply.github.com>
2018-10-18 10:58:57 +0800

    Update print_unicode.py

--- print_unicode.py
+++ print_unicode.py
@@ -1,24 +1,30 @@
 import sys
 import unicodedata
-def	print_unicode_table(word):
+def	print_unicode_table(words):
 	print("decimal hex chr {0:^40}".format("name"))
 	print("…… …… {0:-<40}".format(""))
 
 	code=ord("")
-	end= sys.maxunicode
+	end= min(0xD800, sys.maxunicode)
 
 	while   code<end:
 		c=chr(code)
 		name=unicodedata.name(c,"***unknown***")
-		if word is None or word in name.lower():
-			print("{0:7} {0:5X} {0:^3c} {1}".format(code,name.title()))
-		code+=1
-word=None
+		b = True
+                for word in words:
+                    if word.lower() not in name.lower():
+                        b = False
+                        break
+    
+			if b:
+			   print("{0:7} {0:5X} {0:^3c} {1}".format(code,name.title()))
+		code +=1
+word = []
 if len(sys.argv)>1:
         if sys.argv[1] in ("-h","--help"):
                 print("usage:{0}[string]".format(sys.argv[0]))
-                word =0
+                word = None
         else:
-                word = sys.argv[1].lower()
-        if word!=0:
-                print_unicode_table(word)
+                words = sys.argv[1:]
+        if words is not None:
+             print_unicode_table(words)
