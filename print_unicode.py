739790425eb89437be288fc42b814589299f1e3c
XUARY <43143153+XUARY@users.noreply.github.com>
2018-10-18 10:25:28 +0800

    Update print_unicode.py

--- print_unicode.py
+++ print_unicode.py
@@ -1,24 +1,34 @@
 import sys
 import unicodedata
-def	print_unicode_table(word):
+def	print_unicode_table(words):
 	print("decimal hex chr {0:^40}".format("name"))
 	print("…… …… {0:-<40}".format(""))
 
 	code=ord("")
-	end= sys.maxunicode
+	end= min(0xD800,sys.maxunicode)#Stop at surrogate pairs
 
 	while   code<end:
 		c=chr(code)
 		name=unicodedata.name(c,"***unknown***")
-		if word is None or word in name.lower():
-			print("{0:7} {0:5X} {0:^3c} {1}".format(code,name.title()))
-		code+=1
-word=None
-if len(sys.argv)>1:
-        if sys.argv[1] in ("-h","--help"):
-                print("usage:{0}[string]".format(sys.argv[0]))
-                word =0
-        else:
-                word = sys.argv[1].lower()
-        if word!=0:
-                print_unicode_table(word)
+		 ok = True
+        for word in words:
+            if word not in name.lower():
+                ok = False
+                break
+        if ok:
+            print("{0:7}  {0:5X}  {0:^3c}  {1}".format(
+                  code, name.title()))
+        code += 1
+
+
+words = []
+if len(sys.argv) > 1:
+    if sys.argv[1] in ("-h", "--help"):
+        print("usage: {0} [string1 [string2 [... stringN]]]".format(
+              sys.argv[0]))
+        words = None
+    else:
+        for word in sys.argv[1:]:
+            words.append(word.lower())
+if words is not None:
+    print_unicode_table(words)
